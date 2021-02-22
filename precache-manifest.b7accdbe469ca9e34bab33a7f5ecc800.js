6e232082384590d05ad36e0f3d5b277a45bb18bb
Wangpf <1204981117@qq.com>
2021-02-22 23:19:47 +0800

    deploy

---
+++ precache-manifest.b7accdbe469ca9e34bab33a7f5ecc800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2ad9d86d23b25c46f8",
+    "url": "/bookkeeping-vue-website/css/app.a8200ca7.css"
+  },
+  {
+    "revision": "8e0d9b706d0f8008b00765deb6335cef",
+    "url": "/bookkeeping-vue-website/index.html"
+  },
+  {
+    "revision": "0c2ad9d86d23b25c46f8",
+    "url": "/bookkeeping-vue-website/js/app.cb33cf98.js"
+  },
+  {
+    "revision": "f6e5e001d5ca7fc9aedd",
+    "url": "/bookkeeping-vue-website/js/chunk-vendors.e18fdaba.js"
+  },
+  {
+    "revision": "02adcb277ba6fd882d3307d3966c1370",
+    "url": "/bookkeeping-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookkeeping-vue-website/robots.txt"
+  }
+]);
